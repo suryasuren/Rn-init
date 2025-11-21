92ca5b454e1605520e24de0f3d41bed4d5a871c1
Mariyaselvam <mariyaselvam@squashapps.com>
2025-11-22 01:30:52 +0530

    initial setup for RN

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,65 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+    ecmaFeatures: { jsx: true },
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  env: {
+    'react-native/react-native': true,
+    es6: true,
+    node: true,
+    jest: true
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'react-native',
+    'jsx-a11y'
+  ],
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-native/all',
+    'plugin:jsx-a11y/recommended',
+    'prettier'
+  ],
+  settings: { react: { version: 'detect' } },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': 'error',
+    'prefer-const': 'error',
+    'curly': ['error', 'all'],
+
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/no-unstable-nested-components': ['error', { allowAsProps: false }],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    'react-native/no-inline-styles': 'warn',
+    'react-native/split-platform-components': 'error'
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: { 'no-undef': 'off' }
+    }
+  ]
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
+  printWidth: 100,
+  tabWidth: 2,
+  semi: true,
   singleQuote: true,
   trailingComma: 'all',
-};
+  arrowParens: 'always',
+}
+
+
